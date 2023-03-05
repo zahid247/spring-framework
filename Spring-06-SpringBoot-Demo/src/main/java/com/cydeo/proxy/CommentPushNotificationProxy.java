65d94f38f5695544edd43ba2e6469badfa6787dd
Zahid Aliyev <56856418+zahid247@users.noreply.github.com>
2023-03-05 04:07:10 -0800

    Spring Boot Demo module and the first class for this framework.

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/proxy/CommentPushNotificationProxy.java
@@ -0,0 +1,15 @@
+package com.cydeo.proxy;
+
+import com.cydeo.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("PUSH")
+public class CommentPushNotificationProxy implements CommentNotificationProxy{
+
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending PUSH notification for comment: " + comment.getText());
+    }
+}
